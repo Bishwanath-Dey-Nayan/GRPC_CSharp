76793a1a23a5673c92f6ebb1145db4b042db0d93
Bishwanath-Dey-Nayan <16103022@iubat.edu>
2021-05-26 23:53:46 +0600

    Created new service "StudentService" in gRPC and consumed the data by the Cleint

--- GrpcDemo/GrpcClient/Program.cs
+++ GrpcDemo/GrpcClient/Program.cs
@@ -9,13 +9,18 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var greetClient = new Greeter.GreeterClient(channel);
+
+            //var input = new HelloRequest { Name = "Nayan" };
+            //var reply = await greetClient.SayHelloAsync(input);
+
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var greetClient = new Greeter.GreeterClient(channel);
+            var studentClient = new Student.StudentClient(channel);
 
-            var input = new HelloRequest { Name = "Nayan" };
-            var reply = await greetClient.SayHelloAsync(input);
-
-            Console.WriteLine(reply.Message);
+            var requestedStudent = new StudentLookupModel { StudentId = 1 };
+            var student = await studentClient.GetStudentInfoAsync(requestedStudent);
+            Console.WriteLine($"{ student.FirstName } {student.LastName}");
 
             Console.ReadKey();
         }
---
+++ GrpcDemo/GrpcServer/Services/StudentService.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class StudentService : Student.StudentBase
+    {
+        public StudentService(ILogger<StudentService> logger)
+        {
+            Logger = logger;
+        }
+
+        public ILogger<StudentService> Logger { get; }
+
+        public override Task<StudentModel> GetStudentInfo(StudentLookupModel request, ServerCallContext context)
+        {
+            StudentModel output = new StudentModel();
+            if(request.StudentId == 1)
+            {
+                output.FirstName = "Rahim";
+                output.LastName = "Mia";
+            }
+            else
+            {
+                output.FirstName = "Karim";
+                output.LastName = "Mia";
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
