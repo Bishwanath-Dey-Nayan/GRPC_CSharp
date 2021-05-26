91be0babe91e5bcf63b8080f85d5801e60769bc0
Bishwanath-Dey-Nayan <16103022@iubat.edu>
2021-05-26 23:27:52 +0600

    gRPC server and Client creation with .NET template

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var greetClient = new Greeter.GreeterClient(channel);
+
+            var input = new HelloRequest { Name = "Nayan" };
+            var reply = await greetClient.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadKey();
+        }
+    }
+}
